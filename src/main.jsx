a491180d11a68614b0886159fa73c1b3b5051744
radadiyaansh0-a11y <radadiyaansh0@gmail.com>
2026-08-26 21:08:31 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,15 @@
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 
