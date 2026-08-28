e4f626e4867e383ee7f052cf20b37ebc2adcb7f3
radadiyaansh0-a11y <radadiyaansh0@gmail.com>
2026-08-28 20:24:04 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,7 @@
-import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-function App() {
-  return (
-    <div>
-      <h1>Smora</h1>
-      <p>Our website is starting! 🚀</p>
-    </div>
-  );
-}
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
